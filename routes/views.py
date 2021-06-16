d3d1886d2e90053719e2d3faa3ee7a580e0b0d19
Mike Vickers <mvickers@gcfb.org>
2021-06-15 20:45:31 -0400

    updated home view

--- routes/views.py
+++ routes/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-	return(render(request, 'routes/routes.html'))
+	if request.method == 'POST':
+		context = {'message': 'bad things'}
+	else:
+		context = {'message': 'nothing'}
+	return(render(request, 'routes/routes.html', context))
