43e2cbdb1923e3e983276567e226982a90d39dca
Mike Vickers <mvickers@gcfb.org>
2021-06-17 14:40:06 -0400

    got texts working in a dangerous way

--- routes/views.py
+++ routes/views.py
@@ -1,10 +1,9 @@
+import os
 from django.shortcuts import render
 from .henrys_helpers import (create_route_list, string_box, 
     route_num_to_letter, make_pick_list, validate_arguments)
 from .fulfillment_helper import create_order_dictionary
 
 def home(request):
-	word = 'hot dog'
-	if request.method == 'POST':
-		word = 'taco'
+	word = {'This is a test'}
 	return render(request, 'routes/routes.html', {'key_words': word})
--- texts/views.py
+++ texts/views.py
@@ -1,10 +1,11 @@
+import os
 import json
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from twilio.rest import Client
 
 def home(request):
-    return render(request, 'texts/home.html', {})
+    return render(request, os.path.join('texts', 'home.html'), {})
 
 def send(request):
     with open('twilio_secrets.json') as f:
@@ -12,13 +13,16 @@ def send(request):
     account_sid = secrets['TWILIO_ACCOUNT_SID']
     auth_token = secrets['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
+    pass_hash = request.POST['password']
+    if pass_hash == 'out of bananas':
+        message = client.messages \
+            .create(
+                body = request.POST['text-message'],
+                from_ = '+13132514241',
+                to = '+17342775603'
+            )
 
-    message = client.messages \
-        .create(
-            body = "What up mike?",
-            from_ = '+13132514241',
-            to = '+17342775603'
-        )
-
-    messages.success(request, 'Your text has been sent.')
+        messages.success(request, f'The status of your message was {message.status}.')
+        return redirect('text-home')
+    messages.error(request, 'This was the wrong password')
     return redirect('text-home')
