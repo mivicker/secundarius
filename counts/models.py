2d7a3859702e99aad739623972f159c2a2e5dd68
Mike Vickers <mvickers@gcfb.org>
2021-07-08 14:48:31 -0400

    fixed model error and made migrations

--- counts/models.py
+++ counts/models.py
@@ -1,24 +1,7 @@
 from django.db import models
 
 class Menu(models.Model):
-    box_type = models.ForeignKey("BoxType", on_delete=models.CASCADE)
-    menu_option = models.CharField(max_length = 1, choices = [
-        ('a','A'),
-        ('b','B'),
-        ('c','C'),
-        ('d','D')
-        ])
-    
-    size = models.CharField(max_length = 1, choices = [
-        ('s','Small'),
-        ('m', 'Family'),
-        ('l', 'Large'),
-
-    ])
-
-    class Meta:
-        unique_together = [['box_type', 'menu_option', 'size']]
-
+    description = models.CharField(max_length=50)
     products = models.ManyToManyField("Product", through='Share')
 
     def __str__(self):
@@ -26,8 +9,8 @@ class Menu(models.Model):
 
 class Product(models.Model):
     item_code = models.CharField(max_length = 8)
-    description = models.CharField(max_length = 32)
-    storage = models.CharField(length=32)
+    description = models.CharField(max_length = 50)
+    storage = models.CharField(max_length=32)
     def __str__(self):
         return f"{self.MNMG_ref} - {self.description}"
 
@@ -35,9 +18,3 @@ class Share(models.Model):
     menu = models.ForeignKey(Menu, on_delete = models.CASCADE)
     product = models.ForeignKey(Product, on_delete = models.CASCADE)
     quantity = models.IntegerField(default=1, null=False)
-
-class BoxType(models.Model):
-    description = models.CharField(max_length = 100)
-
-    def __str__(self):
-        return self.description
