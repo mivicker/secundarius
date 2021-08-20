7106f16fad22049e923752d06cc8bf9319ea5002
Mike Vickers <mvickers@gcfb.org>
2021-08-20 15:23:10 -0400

    uncommented the text functions

--- texts/views.py
+++ texts/views.py
@@ -17,9 +17,9 @@ def home(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             words = Broadcast.objects.first()    
-            #client = Client(settings.TWILIO_ACCOUNT_SID,
-            #   settings.TWILIO_AUTH_TOKEN)
-            #texts = send_each(words, read_csv(request.FILES['file']), client)
+            client = Client(settings.TWILIO_ACCOUNT_SID,
+               settings.TWILIO_AUTH_TOKEN)
+            texts = send_each(words, read_csv(request.FILES['file']), client)
 
             messages.success(request, f'Your messages were sent.')
             return redirect('text-home')
