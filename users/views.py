006963a5e161c36feb2654e6278df0ee8d73f64c
Mike Vickers <mvickers@gcfb.org>
2021-09-02 10:54:48 -0400

    worked issues with password template

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def change_password(request):
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('change_password')
+            return redirect('change-password')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
